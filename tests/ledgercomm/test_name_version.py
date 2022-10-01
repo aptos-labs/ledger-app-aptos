498f1efcc67d30ad4e9f3ed4a41a9bbc6d8c1595
vldmkr <vladmakaryan@gmail.com>
2022-10-01 16:19:42 +0300

    Add branding, change tests accordingly

--- tests/ledgercomm/test_name_version.py
+++ tests/ledgercomm/test_name_version.py
@@ -3,5 +3,5 @@ def test_get_app_and_version(cmd, hid):
         # for now it doesn't work with Speculos
         app_name, version = cmd.get_app_and_version()
 
-        assert app_name == "Boilerplate"
-        assert version == "1.0.1"
+        assert app_name == "Aptos"
+        assert version == "0.0.1"
