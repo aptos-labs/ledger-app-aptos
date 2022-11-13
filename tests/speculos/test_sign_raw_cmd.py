902a004da91d872acdcf84f03637686d2011d191
vldmkr <vladmakaryan@gmail.com>
2022-11-13 17:55:32 +0200

    Functional tests refactoring: Implement 'sign_raw_tx' test, cleanup

--- tests/speculos/test_sign_raw_cmd.py
+++ tests/speculos/test_sign_raw_cmd.py
@@ -4,7 +4,7 @@ from nacl.exceptions import BadSignatureError
 from aptos_client.exception import *
 
 
-def test_sign_raw_tx(cmd, button, model):
+def test_sign_raw_tx(cmd, model):
     message = bytes.fromhex("b5e97db07fa0bd0e5598aa3643a9bc6f6693bddc1a9fec9e674a461eaa00b193783135e8b00430253a22ba041d860c373d7a1501ccf7ac2d1ad37a8ed2775aee000000000000000002000000000000000000000000000000000000000000000000000000000000000104636f696e087472616e73666572010700000000000000000000000000000000000000000000000000000000000000010a6170746f735f636f696e094170746f73436f696e000220094c6fc0d3b382a599c37e1aaa7618eff2c96a3586876082c4594c50c50d7dde082a00000000000000204e0000000000006400000000000000565c51630000000022")
     bip32_path: str = "m/44'/637'/1'/0'/0'"
 
@@ -17,7 +17,6 @@ def test_sign_raw_tx(cmd, button, model):
 
     der_sig = cmd.sign_raw(bip32_path=bip32_path,
                            data=message,
-                           button=button,
                            model=model)
 
     try:
