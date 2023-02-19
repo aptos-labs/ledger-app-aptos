9fedf2961871766d967b0d2c1cb2330c141bbe54
vldmkr <vladmakaryan@gmail.com>
2023-02-19 16:47:27 +0200

    Move the version info to the About submenu

--- src/ui/menu.c
+++ src/ui/menu.c
@@ -25,19 +25,17 @@
 
 UX_STEP_NOCB(ux_menu_ready_step, pnn, {&C_aptos_logo, "Aptos", "is ready"});
 UX_STEP_CB(ux_menu_settings_step, pb, ui_menu_settings(), {&C_icon_coggle, "Settings"});
-UX_STEP_NOCB(ux_menu_version_step, bn, {"Version", APPVERSION});
 UX_STEP_CB(ux_menu_about_step, pb, ui_menu_about(), {&C_icon_certificate, "About"});
 UX_STEP_VALID(ux_menu_exit_step, pb, os_sched_exit(-1), {&C_icon_dashboard_x, "Quit"});
 
 // FLOW for the main menu:
 // #1 screen: ready
-// #2 screen: version of the app
+// #2 screen: settings
 // #3 screen: about submenu
 // #4 screen: quit
 UX_FLOW(ux_menu_main_flow,
         &ux_menu_ready_step,
         &ux_menu_settings_step,
-        &ux_menu_version_step,
         &ux_menu_about_step,
         &ux_menu_exit_step,
         FLOW_LOOP);
@@ -55,12 +53,18 @@ void ui_menu_settings() {
 }
 
 UX_STEP_NOCB(ux_menu_info_step, bn, {"Aptos App", "(c) 2022 Pontem"});
+UX_STEP_NOCB(ux_menu_version_step, bn, {"Version", APPVERSION});
 UX_STEP_CB(ux_menu_back_step, pb, ui_menu_main(), {&C_icon_back, "Back"});
 
 // FLOW for the about submenu:
 // #1 screen: app info
-// #2 screen: back button to main menu
-UX_FLOW(ux_menu_about_flow, &ux_menu_info_step, &ux_menu_back_step, FLOW_LOOP);
+// #2 screen: version of the app
+// #3 screen: back button to main menu
+UX_FLOW(ux_menu_about_flow,
+        &ux_menu_info_step,
+        &ux_menu_version_step,
+        &ux_menu_back_step,
+        FLOW_LOOP);
 
 void ui_menu_about() {
     ux_flow_init(0, ux_menu_about_flow, NULL);
